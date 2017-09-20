eca54d5dabf381710e61cc69f4818227b7cf83b8
Justin Daniel <justinddaniel@gmail.com>
2017-09-20 15:53:53 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,26 +4,26 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [:Benvolio => {
+      :hero_friends => [ {
         :name => "Benvolio",
         :age => "17",
         :attitude => "worried"
-        }, :Mercutio => {
+        }, {
           :name => "Mercutio",
           :age => "18",
           :attitude => "hot-headed"
-        }      
+        }
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [:Steven => {
+      :heroine_friends => [ {
         :name => "Steven",
         :age => "30",
         :attitude => "confused"
-        }, :Nurse => {
+        }, {
           :name => "Nurse",
           :age => "44",
           :attitude => "worried"
